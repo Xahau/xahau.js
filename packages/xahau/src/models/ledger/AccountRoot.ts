XRPLF/xrpl.js

feat: add Clawback amendment support (#2353)
* Add Clawback transaction
* Account flag lsfAllowTrustLineClawback
* Support bitwise flag checking of 64 bit flags

packages/xrpl/src/models/ledger/AccountRoot.ts
@@ -142,6 +142,10 @@ export interface AccountRootFlagsInterface {
    * Disallow incoming Trustlines from other accounts.
    */
   lsfDisallowIncomingTrustline?: boolean
+  /**
+   * This address can claw back issued IOUs. Once enabled, cannot be disabled.
+   */
+  lsfAllowTrustLineClawback?: boolean
 }
 
 export enum AccountRootFlags {
@@ -198,4 +202,8 @@ export enum AccountRootFlags {
    * Disallow incoming Trustlines from other accounts.
    */
   lsfDisallowIncomingTrustline = 0x20000000,
+  /**
+   * This address can claw back issued IOUs. Once enabled, cannot be disabled.
+   */
+  lsfAllowTrustLineClawback = 0x80000000,
 }

Xahau/xahau.js

packages/xahau/src/models/ledger/AccountRoot.ts
@@ -152,6 +152,11 @@ export interface AccountRootFlagsInterface {
    * Disallow incoming Remit from other accounts.
    */
   lsfDisallowIncomingRemit?: boolean
+
+  /**
+   * This address can claw back issued IOUs. Once enabled, cannot be disabled.
+   */
+  lsfAllowTrustLineClawback?: boolean
 }
 
 export enum AccountRootFlags {
@@ -216,4 +221,8 @@ export enum AccountRootFlags {
    * Disallow incoming Remits from other accounts.
    */
   lsfDisallowIncomingRemit = 0x80000000,
+  /**
+   * This address can claw back issued IOUs. Once enabled, cannot be disabled.
+   */
+  lsfAllowTrustLineClawback = 0x00001000,
 }
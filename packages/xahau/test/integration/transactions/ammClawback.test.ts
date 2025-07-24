XRPLF/xrpl.js

feat: add AMMClawback support (#2893)
* update definitions and package.json

* update rippled.cfg

* add AMMClawback transaction with unit test

* switch to rippled v2.3.1

* Revert "switch to rippled v2.3.1"

This reverts commit d0bd3bdae97fc82240d5dd18732fa301255dc26a.

* update definitions

* add integ test

* resolve type assertions

* remove package-lock.json in sub packages

* remove ts-expect-error

* update integ test

* console logs

* fix console logs

* add tfClawTwoAssets test and remove logs

* fix lint error

* refactor type assertions

* refactor error messages

* final cleanup

packages/xrpl/test/integration/transactions/ammClawback.test.ts
@@ -1,0 +1,57 @@
+import { AMMClawback, AMMDeposit, AMMDepositFlags, XRP } from 'xrpl'
+
+import serverUrl from '../serverUrl'
+import {
+  setupClient,
+  teardownClient,
+  type XrplIntegrationTestContext,
+} from '../setup'
+import { createAMMPool, testTransaction } from '../utils'
+
+describe('AMMClawback', function () {
+  let testContext: XrplIntegrationTestContext
+
+  beforeAll(async () => {
+    testContext = await setupClient(serverUrl)
+  })
+  afterAll(async () => teardownClient(testContext))
+
+  it('base', async function () {
+    const ammPool = await createAMMPool(testContext.client, true)
+    const { issuerWallet } = ammPool
+    const holderWallet = ammPool.lpWallet
+
+    const asset = {
+      currency: 'USD',
+      issuer: issuerWallet.classicAddress,
+    }
+    const asset2 = {
+      currency: 'XRP',
+    } as XRP
+
+    const ammDepositTx: AMMDeposit = {
+      TransactionType: 'AMMDeposit',
+      Account: holderWallet.classicAddress,
+      Asset: asset,
+      Asset2: asset2,
+      Amount: {
+        currency: 'USD',
+        issuer: issuerWallet.address,
+        value: '10',
+      },
+      Flags: AMMDepositFlags.tfSingleAsset,
+    }
+
+    await testTransaction(testContext.client, ammDepositTx, holderWallet)
+
+    const ammClawback: AMMClawback = {
+      TransactionType: 'AMMClawback',
+      Account: issuerWallet.address,
+      Holder: holderWallet.address,
+      Asset: asset,
+      Asset2: asset2,
+    }
+
+    await testTransaction(testContext.client, ammClawback, issuerWallet)
+  })
+})

packages/xrpl/test/models/AMMClawback.test.ts
@@ -1,0 +1,176 @@
+import { assert } from 'chai'
+
+import { validate, ValidationError } from '../../src'
+import {
+  AMMClawbackFlags,
+  validateAMMClawback,
+} from '../../src/models/transactions/AMMClawback'
+
+/**
+ * AMMClawback Transaction Verification Testing.
+ *
+ * Providing runtime verification testing for each specific transaction type.
+ */
+describe('AMMClawback', function () {
+  let ammClawback
+
+  beforeEach(function () {
+    ammClawback = {
+      TransactionType: 'AMMClawback',
+      Account: 'rWYkbWkCeg8dP6rXALnjgZSjjLyih5NXm',
+      Holder: 'rPyfep3gcLzkosKC9XiE77Y8DZWG6iWDT9',
+      Asset: {
+        currency: 'USD',
+        issuer: 'rWYkbWkCeg8dP6rXALnjgZSjjLyih5NXm',
+      },
+      Asset2: {
+        currency: 'XRP',
+      },
+      Amount: {
+        currency: 'USD',
+        issuer: 'rWYkbWkCeg8dP6rXALnjgZSjjLyih5NXm',
+        value: '1000',
+      },
+      Sequence: 1337,
+    } as any
+  })
+
+  it(`verifies valid AMMClawback`, function () {
+    assert.doesNotThrow(() => validateAMMClawback(ammClawback))
+    assert.doesNotThrow(() => validate(ammClawback))
+  })
+
+  it(`verifies valid AMMClawback without Amount`, function () {
+    delete ammClawback.Amount
+    assert.doesNotThrow(() => validateAMMClawback(ammClawback))
+    assert.doesNotThrow(() => validate(ammClawback))
+  })
+
+  it(`verifies valid AMMClawback with tfClawTwoAssets`, function () {
+    ammClawback.flags = AMMClawbackFlags.tfClawTwoAssets
+    assert.doesNotThrow(() => validateAMMClawback(ammClawback))
+    assert.doesNotThrow(() => validate(ammClawback))
+  })
+
+  it(`throws w/ missing Holder`, function () {
+    delete ammClawback.Holder
+    const errorMessage = 'AMMClawback: missing field Holder'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ invalid field Holder`, function () {
+    ammClawback.Holder = 1234
+    const errorMessage = 'AMMClawback: invalid field Holder'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ Holder and Asset.issuer must be distinct`, function () {
+    ammClawback.Holder = ammClawback.Asset.issuer
+    const errorMessage = 'AMMClawback: Holder and Asset.issuer must be distinct'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ missing Asset`, function () {
+    delete ammClawback.Asset
+    const errorMessage = 'AMMClawback: missing field Asset'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ invalid field Asset`, function () {
+    ammClawback.Asset = '1000'
+    const errorMessage = 'AMMClawback: invalid field Asset'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ Account must be the same as Asset.issuer`, function () {
+    ammClawback.Account = 'rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn'
+    const errorMessage = 'AMMClawback: Account must be the same as Asset.issuer'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ missing Asset2`, function () {
+    delete ammClawback.Asset2
+    const errorMessage = 'AMMClawback: missing field Asset2'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ invalid field Asset2`, function () {
+    ammClawback.Asset2 = '1000'
+    const errorMessage = 'AMMClawback: invalid field Asset2'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ invalid field Amount`, function () {
+    ammClawback.Amount = 1000
+    const errorMessage = 'AMMClawback: invalid field Amount'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ Amount.currency must match Asset.currency`, function () {
+    ammClawback.Amount.currency = 'ETH'
+    const errorMessage =
+      'AMMClawback: Amount.currency must match Asset.currency'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ Amount.issuer must match Amount.issuer`, function () {
+    ammClawback.Amount.issuer = 'rnYgaEtpqpNRt3wxE39demVpDAA817rQEY'
+    const errorMessage = 'AMMClawback: Amount.issuer must match Amount.issuer'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+})

Xahau/xahau.js

packages/xahau/test/integration/transactions/ammClawback.test.ts
@@ -1,0 +1,56 @@
+import { AMMClawback, AMMDeposit, AMMDepositFlags, XAH } from '../../../src'
+import serverUrl from '../serverUrl'
+import {
+  setupClient,
+  teardownClient,
+  type XrplIntegrationTestContext,
+} from '../setup'
+import { createAMMPool, testTransaction } from '../utils'
+
+describe('AMMClawback', function () {
+  let testContext: XrplIntegrationTestContext
+
+  beforeAll(async () => {
+    testContext = await setupClient(serverUrl)
+  })
+  afterAll(async () => teardownClient(testContext))
+
+  it('base', async function () {
+    const ammPool = await createAMMPool(testContext.client, true)
+    const { issuerWallet } = ammPool
+    const holderWallet = ammPool.lpWallet
+
+    const asset = {
+      currency: 'USD',
+      issuer: issuerWallet.classicAddress,
+    }
+    const asset2 = {
+      currency: 'XAH',
+    } as XAH
+
+    const ammDepositTx: AMMDeposit = {
+      TransactionType: 'AMMDeposit',
+      Account: holderWallet.classicAddress,
+      Asset: asset,
+      Asset2: asset2,
+      Amount: {
+        currency: 'USD',
+        issuer: issuerWallet.address,
+        value: '10',
+      },
+      Flags: AMMDepositFlags.tfSingleAsset,
+    }
+
+    await testTransaction(testContext.client, ammDepositTx, holderWallet)
+
+    const ammClawback: AMMClawback = {
+      TransactionType: 'AMMClawback',
+      Account: issuerWallet.address,
+      Holder: holderWallet.address,
+      Asset: asset,
+      Asset2: asset2,
+    }
+
+    await testTransaction(testContext.client, ammClawback, issuerWallet)
+  })
+})

packages/xahau/test/models/AMMClawback.test.ts
@@ -1,0 +1,176 @@
+import { assert } from 'chai'
+
+import { validate, ValidationError } from '../../src'
+import {
+  AMMClawbackFlags,
+  validateAMMClawback,
+} from '../../src/models/transactions/AMMClawback'
+
+/**
+ * AMMClawback Transaction Verification Testing.
+ *
+ * Providing runtime verification testing for each specific transaction type.
+ */
+describe('AMMClawback', function () {
+  let ammClawback
+
+  beforeEach(function () {
+    ammClawback = {
+      TransactionType: 'AMMClawback',
+      Account: 'rWYkbWkCeg8dP6rXALnjgZSjjLyih5NXm',
+      Holder: 'rPyfep3gcLzkosKC9XiE77Y8DZWG6iWDT9',
+      Asset: {
+        currency: 'USD',
+        issuer: 'rWYkbWkCeg8dP6rXALnjgZSjjLyih5NXm',
+      },
+      Asset2: {
+        currency: 'XAH',
+      },
+      Amount: {
+        currency: 'USD',
+        issuer: 'rWYkbWkCeg8dP6rXALnjgZSjjLyih5NXm',
+        value: '1000',
+      },
+      Sequence: 1337,
+    } as any
+  })
+
+  it(`verifies valid AMMClawback`, function () {
+    assert.doesNotThrow(() => validateAMMClawback(ammClawback))
+    assert.doesNotThrow(() => validate(ammClawback))
+  })
+
+  it(`verifies valid AMMClawback without Amount`, function () {
+    delete ammClawback.Amount
+    assert.doesNotThrow(() => validateAMMClawback(ammClawback))
+    assert.doesNotThrow(() => validate(ammClawback))
+  })
+
+  it(`verifies valid AMMClawback with tfClawTwoAssets`, function () {
+    ammClawback.flags = AMMClawbackFlags.tfClawTwoAssets
+    assert.doesNotThrow(() => validateAMMClawback(ammClawback))
+    assert.doesNotThrow(() => validate(ammClawback))
+  })
+
+  it(`throws w/ missing Holder`, function () {
+    delete ammClawback.Holder
+    const errorMessage = 'AMMClawback: missing field Holder'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ invalid field Holder`, function () {
+    ammClawback.Holder = 1234
+    const errorMessage = 'AMMClawback: invalid field Holder'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ Holder and Asset.issuer must be distinct`, function () {
+    ammClawback.Holder = ammClawback.Asset.issuer
+    const errorMessage = 'AMMClawback: Holder and Asset.issuer must be distinct'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ missing Asset`, function () {
+    delete ammClawback.Asset
+    const errorMessage = 'AMMClawback: missing field Asset'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ invalid field Asset`, function () {
+    ammClawback.Asset = '1000'
+    const errorMessage = 'AMMClawback: invalid field Asset'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ Account must be the same as Asset.issuer`, function () {
+    ammClawback.Account = 'rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn'
+    const errorMessage = 'AMMClawback: Account must be the same as Asset.issuer'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ missing Asset2`, function () {
+    delete ammClawback.Asset2
+    const errorMessage = 'AMMClawback: missing field Asset2'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ invalid field Asset2`, function () {
+    ammClawback.Asset2 = '1000'
+    const errorMessage = 'AMMClawback: invalid field Asset2'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ invalid field Amount`, function () {
+    ammClawback.Amount = 1000
+    const errorMessage = 'AMMClawback: invalid field Amount'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ Amount.currency must match Asset.currency`, function () {
+    ammClawback.Amount.currency = 'ETH'
+    const errorMessage =
+      'AMMClawback: Amount.currency must match Asset.currency'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+
+  it(`throws w/ Amount.issuer must match Amount.issuer`, function () {
+    ammClawback.Amount.issuer = 'rnYgaEtpqpNRt3wxE39demVpDAA817rQEY'
+    const errorMessage = 'AMMClawback: Amount.issuer must match Amount.issuer'
+    assert.throws(
+      () => validateAMMClawback(ammClawback),
+      ValidationError,
+      errorMessage,
+    )
+    assert.throws(() => validate(ammClawback), ValidationError, errorMessage)
+  })
+})
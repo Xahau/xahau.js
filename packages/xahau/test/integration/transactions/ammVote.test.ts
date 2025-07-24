682726f2180a72ba0906bd639bc95c0e61bdc0f0
tequ <git@tequ.dev>
2025-07-24 16:49:21 +0900

    fix lint

--- packages/xahau/test/integration/transactions/ammVote.test.ts
+++ packages/xahau/test/integration/transactions/ammVote.test.ts
@@ -79,6 +79,7 @@ describe('AMMVote', function () {
     const beforeDiscountedFee = preDiscountedFee
     const diffDiscountedFee = 7
 
+    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands -- this is valid
     const expectedDiscountedFee = beforeDiscountedFee + diffDiscountedFee
 
     assert.equal(afterTradingFee, expectedTradingFee)

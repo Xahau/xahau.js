2b9dcbbee59f172fd3bc07bc993956cb1886e746
tequ <git@tequ.dev>
2025-07-24 13:34:58 +0900

    fix "Object is of type 'unknown'" error

--- packages/xahau/src/models/transactions/transaction.ts
+++ packages/xahau/src/models/transactions/transaction.ts
@@ -171,9 +171,9 @@ export function validate(transaction: Record<string, unknown>): void {
 
   Object.keys(tx).forEach((key) => {
     const standard_currency_code_len = 3
-    if (tx[key] && isIssuedCurrency(tx[key])) {
-      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions -- needed
-      const txCurrency = (tx[key] as IssuedCurrencyAmount).currency
+    const value = tx[key]
+    if (value && isIssuedCurrency(value)) {
+      const txCurrency = value.currency
 
       if (
         txCurrency.length === standard_currency_code_len &&

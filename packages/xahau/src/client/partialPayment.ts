cd981575095b3881955fbf33f15205fe9f98a9dc
zgrguric <zgrguric@xrplwin.com>
2026-04-04 19:14:43 +0200

    eslint comment to make tests pass
    Added comments to clarify the use of DeliverMax field.

--- packages/xahau/src/client/partialPayment.ts
+++ packages/xahau/src/client/partialPayment.ts
@@ -65,6 +65,9 @@ function isPartialPayment(
 
   const delivered = meta.delivered_amount
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment -- DeliverMax is a valid field on Payment response
+  // @ts-expect-error -- DeliverMax is a valid field on Payment response
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- DeliverMax is a valid field on Payment response
   const amount = tx.DeliverMax ?? tx.Amount
 
   if (delivered === undefined) {

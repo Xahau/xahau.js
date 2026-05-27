21e3a517e808ba99bfc24ffff6ae5cd4f6218fa3
tequ <git@tequ.dev>
2026-05-27 17:15:49 +0900

    Update native claimReward Integration test for IOURewardClaim changes

--- packages/xahau/test/integration/transactions/claimReward.test.ts
+++ packages/xahau/test/integration/transactions/claimReward.test.ts
@@ -1,16 +1,19 @@
 import { assert } from 'chai'
 
-import { ClaimReward, ClaimRewardFlags } from '../../../src'
+import { ClaimReward, ClaimRewardFlags, SetHook } from '../../../src'
 import serverUrl from '../serverUrl'
 import {
   setupClient,
   teardownClient,
   type XrplIntegrationTestContext,
 } from '../setup'
-import { testTransaction } from '../utils'
+import { generateFundedWallet, testTransaction } from '../utils'
 
 // how long before each test case times out
 const TIMEOUT = 20000
+
+const acceptHook =
+  '0061736D0100000001130360027F7F017F60037F7F7E017E60017F017E02170203656E76025F67000003656E760661636365707400010302010205030100020621057F01418088040B7F004180080B7F004180080B7F00418088040B7F004180080B07080104686F6F6B00020A9D80000199800000410141011080808080001A410041004200108180808000'
 
 describe('ClaimReward', function () {
   let testContext: XrplIntegrationTestContext
@@ -23,10 +26,26 @@ describe('ClaimReward', function () {
   it(
     'opt in',
     async () => {
+      const hookWallet = await generateFundedWallet(testContext.client)
+      const setHookTx: SetHook = {
+        TransactionType: 'SetHook',
+        Account: hookWallet.classicAddress,
+        Hooks: [
+          {
+            Hook: {
+              CreateCode: acceptHook,
+              HookApiVersion: 0,
+              HookNamespace: '00'.repeat(32),
+            },
+          },
+        ],
+      }
+      await testTransaction(testContext.client, setHookTx, hookWallet)
+
       const tx: ClaimReward = {
         TransactionType: 'ClaimReward',
         Account: testContext.wallet.classicAddress,
-        Issuer: 'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh',
+        Issuer: hookWallet.classicAddress,
       }
 
       await testTransaction(testContext.client, tx, testContext.wallet)
@@ -42,6 +61,7 @@ describe('ClaimReward', function () {
     },
     TIMEOUT,
   )
+
   it(
     'opt out',
     async () => {

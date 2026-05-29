ae22b7cba8f8c50740bd186f72904ad2a2bb693c
tequ <git@tequ.dev>
2026-05-29 21:16:49 +0900

    update test

--- packages/xahau/test/integration/transactions/claimReward.test.ts
+++ packages/xahau/test/integration/transactions/claimReward.test.ts
@@ -22,15 +22,15 @@ const TIMEOUT = 20000
 const acceptHook =
   '0061736D0100000001130360027F7F017F60037F7F7E017E60017F017E02170203656E76025F67000003656E760661636365707400010302010205030100020621057F01418088040B7F004180080B7F004180080B7F00418088040B7F004180080B07080104686F6F6B00020A9D80000199800000410141011080808080001A4100410042001081808080000B'
 
-const genesisWallet = new Wallet(
-  '0330E7FC9D56BB25D6893BA3F317AE5BCF33B3291BD63DB32654A313222F7FD020',
-  '001ACAAEDECE405B2A958212629E16F2EB46B153EEE94CDD350FDEFF52795525B7',
-)
-
 describe('XAH ClaimReward', function () {
   let testContext: XrplIntegrationTestContext
+  let genesisWallet: Wallet
 
   beforeAll(async () => {
+    genesisWallet = new Wallet(
+      '0330E7FC9D56BB25D6893BA3F317AE5BCF33B3291BD63DB32654A313222F7FD020',
+      '001ACAAEDECE405B2A958212629E16F2EB46B153EEE94CDD350FDEFF52795525B7',
+    )
     testContext = await setupClient(serverUrl)
 
     const setHookTx: SetHook = {
@@ -154,10 +154,10 @@ describe('IOU ClaimReward', function () {
     // reset Hook
     const setHookTx: SetHook = {
       TransactionType: 'SetHook',
-      Account: genesisWallet.classicAddress,
+      Account: hookWallet.classicAddress,
       Hooks: [{ Hook: { CreateCode: '', Flags: { hsfOverride: true } } }],
     }
-    await testTransaction(testContext.client, setHookTx, genesisWallet)
+    await testTransaction(testContext.client, setHookTx, hookWallet)
 
     await teardownClient(testContext)
   })
